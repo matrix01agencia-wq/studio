98ef3aab6453de037f8ed16aa4810dad14f88475
Stanley Ángel <mrstanley0710@gmail.com>
2026-01-21 23:04:12 +0000

    quitar export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
+    /* config options here */
+    };
+
+
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
