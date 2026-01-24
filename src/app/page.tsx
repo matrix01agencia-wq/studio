70a0f42b2326fe40236428cc491fdfbaea1a751a
Stanley Ángel <mrstanley0710@gmail.com>
2026-01-24 16:10:59 +0000

    Solución error 404: agregando página de inicio

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+      return (
+          <main>
+                <h1>App funcionando 🚀</h1>
+                    </main>
+                      )
+                      }
+                      
+}
